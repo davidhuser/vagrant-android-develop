5ec76833693ceece1c5e46ba7a17264f13f387eb
Benjaminhu <info@benjamin.hu>
2015-09-20 11:45:05 +0200

    [add][mod] SOURCES_CDN: cn -> hu

--- provision.d/20-update-system.sh
+++ provision.d/20-update-system.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+SOURCES_CDN=hu
+
 echo "Update system ..."
 
-[[ -f /etc/apt/sources.list.orig ]] || sed -e s,http://archive\.,http://cn.archive\., -i.orig /etc/apt/sources.list
+[[ -f /etc/apt/sources.list.orig ]] || sed -e s,http://archive\.,http://$SOURCES_CDN.archive\., -i.orig /etc/apt/sources.list
 
 apt-get update --fix-missing
 LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get --force-yes -y upgrade
