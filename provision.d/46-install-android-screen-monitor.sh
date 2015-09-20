6568fbe07e87e2c0fc896277ea64d180c6f3b90b
Benjaminhu <info@benjamin.hu>
2015-09-20 17:15:02 +0200

    [add] Install Android Screen Monitor

---
+++ provision.d/46-install-android-screen-monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+ANDROID_SCREEN_MONITOR_FILENAME=ASM_2_50.zip
+
+echo -e "\e[43m\n                    \n   Install Android Screen Monitor ($ANDROID_SCREEN_MONITOR_FILENAME) ...\n\e[0m"
+
+ANDROID_SCREEN_MONITOR_URL=https://android-screen-monitor.googlecode.com/files/$ANDROID_SCREEN_MONITOR_FILENAME
+ANDROID_SCREEN_MONITOR_LOCAL_CACHE=/vagrant/cache/$ANDROID_SCREEN_MONITOR_FILENAME
+
+[[ -e $ANDROID_SDK_LOCAL_CACHE ]] || \
+wget --quiet -O $ANDROID_SCREEN_MONITOR_LOCAL_CACHE $ANDROID_SCREEN_MONITOR_URL
+
+unzip -o $ANDROID_SCREEN_MONITOR_LOCAL_CACHE -d $ANDROID_HOME
