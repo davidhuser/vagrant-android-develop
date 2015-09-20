1ef0f2331d204169eba362ed7bc1a6460b7a815e
Benjaminhu <info@benjamin.hu>
2015-09-20 11:44:34 +0200

    [add][mod] TIMEZONE & LOCALE

--- provision.d/00-timezone-locale.sh
+++ provision.d/00-timezone-locale.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
-timedatectl set-timezone Asia/Chongqing
-locale --all-locales | fgrep zh_CN.utf8 > /dev/null || locale-gen zh_CN.utf8
+TIMEZONE=Europe/Budapest
+LOCALE=hu_HU.utf8
+
+timedatectl set-timezone $TIMEZONE
+locale --all-locales | fgrep $LOCALE > /dev/null || locale-gen $LOCALE
