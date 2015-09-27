00b25912f2de3b2ff9b92630501a3bc364e96117
Benjaminhu <info@benjamin.hu>
2015-09-27 23:00:41 +0200

    [add] CALABASH_ANDROID_VERSION [mod] version 0.5.5 -> 0.5.14

--- provision.d/50-install-cucumber.sh
+++ provision.d/50-install-cucumber.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-echo -e "\e[43m\n                    \n   Install Calabash for Android ...\n\e[0m"
+CALABASH_ANDROID_VERSION=0.5.14
+
+echo -e "\e[43m\n                    \n   Install Calabash for Android ($CALABASH_ANDROID_VERSION) ...\n\e[0m"
 
 su -lc /bin/bash vagrant <<EOF
   set -e
@@ -12,5 +14,5 @@ su -lc /bin/bash vagrant <<EOF
   source \$HOME/.rvm/scripts/rvm
   rvm list | fgrep ruby || LC_ALL=C DEBIAN_FRONTEND=noninteractive rvm --quiet-curl install ruby
   rvm use ruby@Calabash-Android --create --default
-  gem install calabash-android --version 0.5.5
+  gem install calabash-android --version $CALABASH_ANDROID_VERSION
 EOF
