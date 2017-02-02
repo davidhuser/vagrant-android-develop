34a63b70dacc27cace8e1d5c69a7acb9cea00e8d
David Huser <dhuser@baosystems.com>
2017-02-02 14:02:33 +0100

    Update 30-install-develop-tools.sh

--- provision.d/30-install-develop-tools.sh
+++ provision.d/30-install-develop-tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get --force-yes -y install git
+LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get --force-yes -y install git xdg-utils gvfs-bin
 wget https://github.com/atom/atom/releases/download/v1.13.1/atom-amd64.deb
 dpkg -i atom-amd64.deb
