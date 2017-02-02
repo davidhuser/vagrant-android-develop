6b00ddedc22664503c39b5dc415642248b1193c6
David Huser <dhuser@baosystems.com>
2017-02-02 12:27:40 +0100

    Update 00-timezone-locale.sh

--- provision.d/00-timezone-locale.sh
+++ provision.d/00-timezone-locale.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-TIMEZONE=Europe/Budapest
-LOCALE=hu_HU.utf8
+TIMEZONE=Atlantic/Reykjavik
+LOCALE=en_US.utf8
 
 echo -e "\e[43m\n                    \n   Set timezone: $TIMEZONE locale: $LOCALE ...\n\e[0m"
 
