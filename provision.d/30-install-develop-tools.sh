c700b85ca6e28e02d59f9201d885af2c594e628d
David Huser <dhuser@baosystems.com>
2017-02-02 13:53:34 +0100

    Update 30-install-develop-tools.sh

--- provision.d/30-install-develop-tools.sh
+++ provision.d/30-install-develop-tools.sh
@@ -2,3 +2,5 @@
 set -e
 
 LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get --force-yes -y install git
+wget https://github.com/atom/atom/releases/download/v1.13.1/atom-amd64.deb
+dpkg -i atom-amd64.deb
