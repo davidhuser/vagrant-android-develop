2bb6d290e3fd0e0eb1bf2a62534832f02c503174
David Huser <dhuser@baosystems.com>
2017-02-02 15:10:44 +0100

    Update 30-install-develop-tools.sh

--- provision.d/30-install-develop-tools.sh
+++ provision.d/30-install-develop-tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get --force-yes -y install git xdg-utils gvfs-bin chromium-browser
+LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get --force-yes -y install git xdg-utils gvfs-bin chromium-browser libc6:i386 libcurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
 wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb
 dpkg -i sublime-text_build-3126_amd64.deb
