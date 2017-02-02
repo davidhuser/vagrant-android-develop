e697b72569e6cbd774f2142189eb533981d8fcc3
David Huser <dhuser@baosystems.com>
2017-02-02 12:52:03 +0100

    Update 46-install-android-screen-monitor.sh

--- provision.d/46-install-android-screen-monitor.sh
+++ provision.d/46-install-android-screen-monitor.sh
@@ -5,7 +5,7 @@ ANDROID_SCREEN_MONITOR_FILENAME=ASM_2_50.zip
 
 echo -e "\e[43m\n                    \n   Install Android Screen Monitor ($ANDROID_SCREEN_MONITOR_FILENAME) ...\n\e[0m"
 
-ANDROID_SCREEN_MONITOR_URL=https://android-screen-monitor.googlecode.com/files/$ANDROID_SCREEN_MONITOR_FILENAME
+ANDROID_SCREEN_MONITOR_URL=https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/android-screen-monitor/$ANDROID_SCREEN_MONITOR_FILENAME
 ANDROID_SCREEN_MONITOR_LOCAL_CACHE=/vagrant/cache/$ANDROID_SCREEN_MONITOR_FILENAME
 
 [[ -e $ANDROID_SDK_LOCAL_CACHE ]] || \
