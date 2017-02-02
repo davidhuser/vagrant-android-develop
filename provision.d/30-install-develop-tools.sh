fa97e17c731fe5a3db94283dc6bdd1cf7dfda890
David Huser <dhuser@baosystems.com>
2017-02-02 14:08:43 +0100

    Update 30-install-develop-tools.sh

--- provision.d/30-install-develop-tools.sh
+++ provision.d/30-install-develop-tools.sh
@@ -2,5 +2,5 @@
 set -e
 
 LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get --force-yes -y install git xdg-utils gvfs-bin
-wget https://github.com/atom/atom/releases/download/v1.13.1/atom-amd64.deb
-dpkg -i atom-amd64.deb
+wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb
+dpkg -i sublime-text_build-3126_amd64.deb
