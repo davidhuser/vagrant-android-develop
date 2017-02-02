b660c1d755962529cb4eeb7610cc907df0e8de15
David Huser <dhuser@baosystems.com>
2017-02-02 16:12:47 +0100

    Update 40-install-jdk.sh

--- provision.d/40-install-jdk.sh
+++ provision.d/40-install-jdk.sh
@@ -3,16 +3,12 @@ set -e
 
 echo -e "\e[43m\n                    \n   Install JDK ...\n\e[0m"
 
-apt-get install -y openjdk-7-jre
+add-apt-repository ppa:webupd8team/java -y
+apt-get update
+LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get --force-yes -y install oracle-java8-installer
+LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get --force-yes -y install oracle-java8-set-default
 
-case $(uname -i) in
-    i386|i686)
-        JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386
-        ;;
-    *)
-        JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
-        ;;
-esac
+JAVA_HOME=/usr/lib/jvm/java-8-oracle
 
 cat > /etc/profile.d/java.sh <<EOF
 export JAVA_HOME=$JAVA_HOME
