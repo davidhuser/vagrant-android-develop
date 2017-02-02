bbe8d348526daaea1879256d45abd33d7c78fbab
David Huser <dhuser@baosystems.com>
2017-02-02 12:36:22 +0100

    Update 40-install-jdk.sh

--- provision.d/40-install-jdk.sh
+++ provision.d/40-install-jdk.sh
@@ -3,14 +3,14 @@ set -e
 
 echo -e "\e[43m\n                    \n   Install JDK ...\n\e[0m"
 
-apt-get install -y openjdk-8-jre
+apt-get install -y openjdk-7-jre
 
 case $(uname -i) in
     i386|i686)
-        JAVA_HOME=/usr/lib/jvm/java-8-openjdk-i386
+        JAVA_HOME=/usr/lib/jvm/java-7-openjdk-i386
         ;;
     *)
-        JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+        JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
         ;;
 esac
 
